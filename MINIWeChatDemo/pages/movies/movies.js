08078ef99d15e3d3586e4ec7af756dd67be915bb
Song <784341699@qq.com>
2019-07-02 16:48:48 +0800

    电影详情页初步
    电影详情页初步

--- MINIWeChatDemo/pages/movies/movies.js
+++ MINIWeChatDemo/pages/movies/movies.js
@@ -1,17 +1,33 @@
 // pages/movies/movies.js
+
+const MOVIE_URL = 'http://t.yushu.im/v2/movie/top250';
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    moviesArr: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
+    // 发送请求
+    wx.request({
+      url: MOVIE_URL,
+      success: (data) => {
+        console.log(data);
+        // 更新状态值
+        this.setData({
+          moviesArr:data.data.subjects
+        });
+      }
+    })
+
+
 
   },
 

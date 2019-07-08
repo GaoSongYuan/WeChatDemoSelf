0ba1e6266bf319f43a4dac6e942e9395124a91cb
Song <784341699@qq.com>
2019-07-08 16:10:20 +0800

    封装http类
    封装http类

--- 7yueeee/pages/classic/classic.js
+++ 7yueeee/pages/classic/classic.js
@@ -1,28 +1,37 @@
 // pages/classic/classic.js
+
+import {HTTP} from '../../util/http.js'
+let http = new HTTP()
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    test : 1
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-    console.log("ddd" + this.data.test)
-    
-    wx.request({
-      url: 'http://bl.7yue.pro/v1/classic/latest',
-      header:{
-        appkey:"RdshydjBvcYZhMZC"
-      },
-      success:(res) => {
-        console.log(this.data.test)
+    http.request({
+      url:'classic/latest',
+      success:(res)=> {
+        console.log(res)
       }
     })
+
+    // wx.request({
+    //   url: 'http://bl.7yue.pro/v1/classic/latest',
+    //   header:{
+    //     appkey:"RdshydjBvcYZhMZC"
+    //   },
+    //   success:(res) => {
+    //     console.log(this.data.test)
+    //   }
+    // })
   },
 
   /**
--- 7yueeee/util/http.js
+++ 7yueeee/util/http.js
@@ -1,4 +1,10 @@
-import {config,fun1} from '/config.js'
+import {config,fun1} from '../config.js'
+
+const tips = {
+  1:'抱歉，出现了一个错误',
+  1005: 'appkey无效',
+  3000: '期刊不存在'
+}
 
 class HTTP {
   request(params) {
@@ -15,17 +21,29 @@ class HTTP {
         'appkey' : config.appkey
       },
       success:(res) => {
-        let code = res.statusCode
+        let code = res.statusCode.toString()
         if(code.startsWith('2')) {
-
+          params.success(res.data)
         }
         else {
-
+          let error_code = res.data.error_code
+          this._show_error(error_code)
         }
       },
       fail:(err) => {
-
+        this._show_error(1)
       }
+    })
+  }
+
+  _show_error(error_code) {
+    if(!error_code) {
+      error_code = 1;
+    }
+    wx.showToast({
+      title: tips[error_code],
+      icon: 'none',
+      duration: 2000
     })
   }
 }
